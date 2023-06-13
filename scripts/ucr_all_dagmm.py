297e881f644a08703c40781e5e60e7df02a7e84c
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2023-06-13 16:47:37 -0300

    fix: dagmm implementation

--- scripts/ucr_all_dagmm.py
+++ scripts/ucr_all_dagmm.py
@@ -6,8 +6,26 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
+import warnings
+warnings.filterwarnings('ignore')
 from torch.nn import functional as F
 import math
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+
+class BaseDataset(Dataset):
+    def __init__(self, x, y) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+        
+        assert len(self.x) == len(self.y)
+        
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        return torch.from_numpy(self.x[index]).float(), torch.from_numpy(np.array([self.y[index]]))
 
 
 class DAGMM(pl.LightningModule):
@@ -74,7 +92,7 @@ class DAGMM(pl.LightningModule):
         return z_c, x_hat, z, gamma
     
     def reconstruction_error(self, x: torch.Tensor, x_hat: torch.Tensor) -> torch.Tensor:
-        e = torch.Tensor(0.0)
+        e = torch.tensor(0.0).to(self.device)
         for i in range(x.shape[0]):
             e += torch.dist(x[i], x_hat[i])
         return e / x.shape[0]
@@ -106,14 +124,14 @@ class DAGMM(pl.LightningModule):
         n_gmm: torch.Tensor,
         bs: torch.Tensor
     ) -> torch.Tensor:
-        e = torch.tensor(0.0)
-        cov_eps = torch.eye(mean.shape[1]) * (1e-12)
+        e = torch.tensor(0.0).to(self.device)
+        cov_eps = (torch.eye(mean.shape[1]) * (1e-12)).to(self.device)
 
         for k in range(n_gmm):
             miu_k = mean[k].unsqueeze(1)
             d_k = zi - miu_k
             
-            inv_cov = torch.inverse(cov[k] + cov_eps)
+            inv_cov = torch.inverse(cov[k] + cov_eps).to(self.device)
             e_k = torch.exp(-0.5 * torch.chain_matmul(torch.t(d_k), inv_cov, d_k))
             e_k = e_k / torch.sqrt(torch.abs(torch.det(2 * math.pi * cov[k])))
             e_k = e_k * ceta[k]
@@ -129,7 +147,7 @@ class DAGMM(pl.LightningModule):
         z: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         bs, n_gmm = gamma.shape[0], gamma.shape[1]
-        
+
         # Reconstruction error
         reconstruction_error = self.reconstruction_error(x, x_hat)
         
@@ -137,13 +155,13 @@ class DAGMM(pl.LightningModule):
         ceta, mean, cov = self.get_gmm_param(gamma, z)
         
         # 3nd Loss Part
-        e = torch.tensor(0.0)
+        e = torch.tensor(0.0).to(self.device)
         for i in range(z.shape[0]):
             z_i = z[i].unsqueeze(1)
-            e_i = self.sample_energy(ceta, mean, cov, z_i, n_gmm, bs)
+            e_i = self.sample_energy(ceta, mean, cov, z_i, n_gmm, bs).to(self.device)
             e += e_i
             
-        p = torch.tensor(0.0)
+        p = torch.tensor(0.0).to(self.device)
         for k in range(n_gmm):
             cov_k = cov[k]
             p_k = torch.sum(1 / torch.diagonal(cov_k, 0))
@@ -174,7 +192,8 @@ class DAGMM(pl.LightningModule):
         threshold = np.percentile(energies, 80)
         return threshold
     
-    def set_threshold(self, )
+    def set_threshold(self, dataloader: DataLoader, len_data: int) -> None:
+        self.threshold = self.compute_threshold(dataloader, len_data)
 
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
         x, labels = batch
@@ -191,4 +210,210 @@ class DAGMM(pl.LightningModule):
         return loss
     
     def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
+        assert self.threshold != None, 'You must set an threshold before perform test steps.'
+
+        x, y = batch
+        scores = np.zeros(shape=(len(x), 2))
+        step = 0
+        
+        z_c, x_hat, z, gamma = self(x)
+        m_prob, m_mean, m_cov = self.get_gmm_param(gamma, z)
+
+        # For each instance in the batch
+        for i in range(z.shape[0]):
+            z_i = z[i].unsqueeze(1)
+            sample_energy = self.sample_energy(m_prob, m_mean, m_cov, z_i, gamma.shape[1], gamma.shape[0])
+            se = sample_energy.detach().item()
+            
+            scores[step] = [int(y[i]), int(se > self.threshold)]
+            step += 1
+
+        accuracy = accuracy_score(scores[:, 0], scores[:, 1])
+        precision, recall, f1, support = precision_recall_fscore_support(scores[:, 0], scores[:, 1], average='binary')
+
+        self.log('accuracy', accuracy, on_epoch=True, on_step=False, prog_bar=True)
+        self.log('f1', f1, on_epoch=True, on_step=False, prog_bar=True)
+        self.log('recall', recall, on_epoch=True, on_step=False, prog_bar=True)
+        self.log('precision', precision, on_epoch=True, on_step=False, prog_bar=True)
+
         return
+
+
+UCR_DATASETS = [
+    'Adiac',
+    'ArrowHead',
+    'Beef',
+    'BeetleFly',
+    'BirdChicken',
+    'Car',
+    'CBF',
+    'ChlorineConcentration',
+    'CinCECGTorso',
+    'Coffee',
+    'Computers',
+    'CricketX',
+    'CricketY',
+    'CricketZ',
+    'DiatomSizeReduction',
+    'DistalPhalanxOutlineAgeGroup',
+    'DistalPhalanxOutlineCorrect',
+    'DistalPhalanxTW',
+    'Earthquakes',
+    'ECG200',
+    'ECG5000',
+    'ECGFiveDays',
+    'ElectricDevices',
+    'FaceAll',
+    'FaceFour',
+    'FacesUCR',
+    'FiftyWords',
+    'Fish',
+    'FordA',
+    'FordB',
+    'GunPoint',
+    'Ham',
+    'HandOutlines',
+    'Haptics',
+    'Herring',
+    'InlineSkate',
+    'InsectWingbeatSound',
+    'ItalyPowerDemand',
+    'LargeKitchenAppliances',
+    'Lightning2',
+    'Lightning7',
+    'Mallat',
+    'Meat',
+    'MedicalImages',
+    'MiddlePhalanxOutlineAgeGroup',
+    'MiddlePhalanxOutlineCorrect',
+    'MiddlePhalanxTW',
+    'MoteStrain',
+    'NonInvasiveFetalECGThorax1',
+    'NonInvasiveFetalECGThorax2',
+    'OliveOil',
+    'OSULeaf',
+    'PhalangesOutlinesCorrect',
+    'Phoneme',
+    'Plane',
+    'ProximalPhalanxOutlineAgeGroup',
+    'ProximalPhalanxOutlineCorrect',
+    'ProximalPhalanxTW',
+    'RefrigerationDevices',
+    'ScreenType',
+    'ShapeletSim',
+    'ShapesAll',
+    'SmallKitchenAppliances',
+    'SonyAIBORobotSurface1',
+    'SonyAIBORobotSurface2',
+    'StarLightCurves',
+    'Strawberry',
+    'SwedishLeaf',
+    'Symbols',
+    'SyntheticControl',
+    'ToeSegmentation1',
+    'ToeSegmentation2',
+    'Trace',
+    'TwoLeadECG',
+    'TwoPatterns',
+    'UWaveGestureLibraryAll',
+    'UWaveGestureLibraryX',
+    'UWaveGestureLibraryY',
+    'UWaveGestureLibraryZ',
+    'Wafer',
+    'Wine',
+    'WordSynonyms',
+    'Worms',
+    'WormsTwoClass',
+    'Yoga',
+    'ACSF1',
+    'BME',
+    'Chinatown',
+    'Crop',
+    'EOGHorizontalSignal',
+    'EOGVerticalSignal',
+    'EthanolLevel',
+    'FreezerRegularTrain',
+    'FreezerSmallTrain',
+    'Fungi',
+    'GunPointAgeSpan',
+    'GunPointMaleVersusFemale',
+    'GunPointOldVersusYoung',
+    'HouseTwenty',
+    'InsectEPGRegularTrain',
+    'InsectEPGSmallTrain',
+    'MixedShapesRegularTrain',
+    'MixedShapesSmallTrain',
+    'PigAirwayPressure',
+    'PigArtPressure',
+    'PigCVP',
+    'PowerCons',
+    'Rock',
+    'SemgHandGenderCh2',
+    'SemgHandMovementCh2',
+    'SemgHandSubjectCh2',
+    'SmoothSubspace',
+    'UMD'
+]
+
+results = {
+    'dataset': [],
+    'model': [],
+    'label': [],
+    'accuracy': [],
+    'f1': [],
+    'recall': [],
+    'precision': [],
+}
+
+for dataset in UCR_DATASETS:
+    print(f'Starting experiments with {dataset} dataset...')
+    # Load the data from .tsv files
+    train_data = np.genfromtxt(f'../data/ucr/{dataset}/{dataset}_TRAIN.tsv')
+    x_train, y_train = train_data[:, 1:], train_data[:, 0]
+    
+    test_data = np.genfromtxt(f'../data/ucr/{dataset}/{dataset}_TEST.tsv')
+    x_test, y_test = test_data[:, 1:], test_data[:, 0]
+    
+    unique_labels = np.unique(y_train)
+    for label in unique_labels:
+        print(f'\tClassifying the label {label}...')
+        # Filter samples from positive label
+        x_train_ = x_train[y_train == label]
+        y_train_ = y_train[y_train == label]
+
+        # In the case of dagmm the normal class is listed as 0 and abnormal as 1
+        y_test_ = np.array([0 if y_true == label else 1 for y_true in y_test])
+
+        # Apply z normalization
+        std_ = x_train_.std(axis=1, keepdims=True)
+        std_[std_ == 0] = 1.0
+        x_train_ = (x_train_ - x_train_.mean(axis=1, keepdims=True)) / std_
+        
+        std_ = x_test.std(axis=1, keepdims=True)
+        std_[std_ == 0] = 1.0
+        x_test_ = (x_test - x_test.mean(axis=1, keepdims=True)) / std_
+
+        train_set = BaseDataset(x=x_train_, y=y_train_)
+        test_set = BaseDataset(x=x_test_, y=y_test_)
+        
+        train_loader = DataLoader(train_set, batch_size=16)
+        test_loader = DataLoader(test_set, batch_size=16)
+
+        dagmm = DAGMM(x_train_.shape[-1])
+        trainer = pl.Trainer(max_epochs=300, accelerator='gpu', devices=-1)
+        trainer.fit(dagmm, train_dataloaders=train_loader)
+        
+        dagmm.set_threshold(train_loader, len(train_set))
+
+        metrics = trainer.test(dagmm, dataloaders=test_loader)[0]
+
+        results['dataset'].append(dataset)
+        results['model'].append('dagmm')
+        results['label'].append(label)
+        results['accuracy'].append(metrics['accuracy'])
+        results['f1'].append(metrics['f1'])
+        results['recall'].append(metrics['recall'])
+        results['precision'].append(metrics['precision'])
+
+metrics = pd.DataFrame(results)
+metrics.to_csv('./ucr_dagmm.csv', index=False)
